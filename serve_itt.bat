e0c06be35919858c4a126400ce37bcbe9331eb81
wl239 <liwanting12345@gmail.com>
2022-04-17 18:17:15 -0400

    Add Webhook

---
+++ serve_itt.bat
@@ -0,0 +1,4 @@
+cd C:\Users\wl239\PycharmProjects\interactive_trader
+git pull https://%TESTAPP_GIT_PAT%@github.com/FT533-Magikarp/interactive_trader.git
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
