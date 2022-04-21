b597649b177fcda949a49e619362796f4e8a8571
wl239 <liwanting12345@gmail.com>
2022-04-20 20:53:14 -0400

    Action Table

--- data.py
+++ data.py
@@ -17,7 +17,7 @@ def get_bolling_band(df, n, k, stockA, stockB):
     upper_band = []
     lower_band = []
     spreads = df["spread"]
-    series = df[[stockA+"_Open", stockB+"_Open", "spread"]]
+    series = df[[stockA + "_Open", stockB + "_Open", "spread"]]
     for spread in spreads:
         history.append(spread)
         if len(history) > n:
@@ -37,10 +37,10 @@ def get_signal(df):
     spread = df["spread"]
     upper_band = df["upper_band"]
     lower_band = df["lower_band"]
-    for i in range(len(df)-1):
-        if spread[i] > upper_band[i] and spread[i+1] <= upper_band[i+1]:
+    for i in range(len(df) - 1):
+        if spread[i] > upper_band[i] and spread[i + 1] <= upper_band[i + 1]:
             signal.append("x_up")
-        elif spread[i] < lower_band[i] and spread[i+1] >= upper_band[i+1]:
+        elif spread[i] < lower_band[i] and spread[i + 1] >= upper_band[i + 1]:
             signal.append("x_down")
         else:
             signal.append("false")
@@ -54,11 +54,59 @@ stockB = "ko"
 
 data_after_cal = get_data_csv("pep_ko_ivv.csv", stockA, stockB)
 
-bolling_data = get_bolling_band(data_after_cal, 7, 0.5, stockA, stockB)
+bolling_data = get_bolling_band(data_after_cal, 7, 2, stockA, stockB)
 
 signal_data = get_signal(bolling_data)
 
 series1 = signal_data.loc[signal_data["signal"] != "false"]
 
-print(series1)
+length = len(series1)
 
+file = pd.read_csv("Action.csv")
+
+for i in range(len(series1)):
+    if series1.iloc[length - 1 - i]['signal'] == 'x_up':
+        file = pd.concat(
+            [file, pd.DataFrame({
+                "DATE": [series1.index[length - 1 - i]],
+                "SYMBOL1": ['PEP'],
+                "ACTION": ['SELL'],
+                "SIZE": ['1000'],
+                "PRICE": [series1.iloc[length - 1 - i]['pep_Open']],
+                "TRIP": ['Entry'],
+                "LMT_PRICE": ['N/A'],
+                "STATUS": ['Filled'],
+
+                "SYMBOL2": ['KO'],
+                "ACTION2": ['BUY'],
+                "SIZE2": ['1000'],
+                "PRICE2": [series1.iloc[length - 1 - i]['ko_Open']],
+                "TRIP2": ['Entry'],
+                "LMT_PRICE2": ['N/A'],
+                "STATUS2": ['Filled'],
+            })])
+    elif series1.iloc[length - 1 - i]['signal'] == 'x_down':
+        file = pd.concat(
+            [file, pd.DataFrame({
+                "DATE": [series1.index[length - 1 - i]],
+                "SYMBOL1": ['PEP'],
+                "ACTION": ['BUY'],
+                "SIZE": ['1000'],
+                "PRICE": [series1.iloc[length - 1 - i]['pep_Open']],
+                "TRIP": ['Entry'],
+                "LMT_PRICE": ['N/A'],
+                "STATUS": ['Filled'],
+
+                "SYMBOL2": ['KO'],
+                "ACTION2": ['SELL'],
+                "SIZE2": ['1000'],
+                "PRICE2": [series1.iloc[length - 1 - i]['ko_Open']],
+                "TRIP2": ['Entry'],
+                "LMT_PRICE2": ['N/A'],
+                "STATUS2": ['Filled'],
+            })])
+
+file.to_csv("Action2.csv")
+
+# print(series1.index[0])
+print(file)
