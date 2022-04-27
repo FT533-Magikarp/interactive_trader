bdace086eb6aeb1f3a6801c7f14305f078e87105
JakeVestal <jmvestal@gmail.com>
2022-04-27 09:56:34 -0400

    jake_edit

--- data.py
+++ data.py
@@ -4,12 +4,17 @@ import numpy as np
 
 
 def get_data_csv(filename):
+    # reads a csv that has a date column and returns it as a pandas DF
     df = pd.read_csv(filename, index_col="Date")
     df.sort_index(ascending=True, inplace=True)
     return df
 
 
 def get_spread(filename, stock_a, stock_b):
+    # adds three new columns to a csv of date-indexed prices:
+    # t_price_A: high+low+close price of stock A / 3
+    # t_price_B: high+low+close price of stock B / 3
+    # spread: difference between t-price of A wrt B
     df = get_data_csv(filename)
     df["t_price_A"] = (df[stock_a + "_High"] + df[stock_a + "_Low"] + df[stock_a + "_Close"]) / 3
     df["t_price_B"] = (df[stock_b + "_High"] + df[stock_b + "_Low"] + df[stock_b + "_Close"]) / 3
@@ -18,6 +23,10 @@ def get_spread(filename, stock_a, stock_b):
 
 
 def get_bolling_band(filename, n, k, stock_a, stock_b):
+    # Accepts the filename of a date-indexed CSV of historical prices, plus
+    #   n and k, plus the symbols of two stocks A and B
+    # Returns a pandas DF containing:
+    # t_price_a, t_price_b, spread, upper_band, lower_band
     df = get_spread(filename, stock_a, stock_b)
     history = []
     upper_band = []
@@ -38,6 +47,9 @@ def get_bolling_band(filename, n, k, stock_a, stock_b):
 
 
 def get_full_signal(filename, n, k, stock_a, stock_b):
+    # takes in the same params as above
+    # returns a date-indexed pandas df, columns:
+    #   pep_Open, ko_open, spread, upper_band, lower_band, signal
     df = get_bolling_band(filename, n, k, stock_a, stock_b)
     signal = []
     df.sort_index(ascending=False, inplace=True)
@@ -56,11 +68,13 @@ def get_full_signal(filename, n, k, stock_a, stock_b):
     return df
 
 
-def get_table(filename, n, k, stock_a, stock_b, size_a, size_b, trip,
-              lmt_price_a, lmt_status_a, lmt_price_b, lmt_status_b):
+def calculate_entry_orders(filename, n, k, stock_a, stock_b, size_a, size_b,
+                           trip, lmt_price_a, lmt_status_a, lmt_price_b,
+                           lmt_status_b):
+    # returns a blotter containing all entry orders given a set of data.
     df = get_full_signal(filename, n, k, stock_a, stock_b)
     df = df.reset_index()
-    series = df[["Date", stockA + "_Open", stockB + "_Open"]]
+    series = df[["Date", stock_a + "_Open", stock_b + "_Open"]]
     signal = df["signal"]
     series = series.drop(series.index[-1]).reset_index(drop=True)
     signal = signal.drop(signal.index[0]).reset_index(drop=True)
@@ -71,62 +85,77 @@ def get_table(filename, n, k, stock_a, stock_b, size_a, size_b, trip,
 
     entry = entry.sort_index(ascending=True)
 
-    columns = ["DATE", "SYMBOL1", "ACTION", "SIZE", "PRICE", "TRIP", "LMT_PRICE", "STATUS",
-               "SYMBOL2", "ACTION2", "SIZE2", "PRICE2", "TRIP2", "LMT_PRICE2", "STATUS2"]
-    file = pd.DataFrame(columns=columns)
+    entry_blotter = pd.DataFrame(columns=["DATE", "SYMBOL", "ACTION", "SIZE",
+                                          "PRICE", "TRIP", "LMT_PRICE",
+                                          "STATUS"])
 
     position = 0
     for i in range(len(entry)):
         # up: buy ko, sell pepsi (buy B (low), sell A (high))
         if position == 0 and signal[i] == "x_up":
             position = 1
-            file = pd.concat(
-                [file, pd.DataFrame(
+            entry_blotter = pd.concat(
+                [entry_blotter, pd.DataFrame(
                     {
                         "DATE": [entry.index[i]],
-                        "SYMBOL1": [stock_a],
+                        "SYMBOL": [stock_a],
                         "ACTION": ['SELL'],
                         "SIZE": [size_a],
                         "PRICE": [entry.iloc[i][stock_a+"_Open"]],
                         "TRIP": [trip],
                         "LMT_PRICE": [lmt_price_a],
-                        "STATUS": [lmt_status_a],
-                        "SYMBOL2": [stock_b],
-                        "ACTION2": ['BUY'],
-                        "SIZE2": [size_b],
-                        "PRICE2": [entry.iloc[i][stock_b + "_Open"]],
-                        "TRIP2": [trip],
-                        "LMT_PRICE2": [lmt_price_b],
-                        "STATUS2": [lmt_status_b],
+                        "STATUS": [lmt_status_a]
                     }
                 )]
             )
+            entry_blotter = pd.concat(
+                [entry_blotter, pd.DataFrame(
+                    {
+                        "DATE": [entry.index[i]],
+                        "SYMBOL": [stock_b],
+                        "ACTION": ['BUY'],
+                        "SIZE": [size_b],
+                        "PRICE": [entry.iloc[i][stock_b + "_Open"]],
+                        "TRIP": [trip],
+                        "LMT_PRICE": [lmt_price_b],
+                        "STATUS": [lmt_status_b]
+                    }
+                )]
+            )
+
         # down: buy pepsi, sell ko (buy high, sell low)
         elif position == 1 and signal[i] == "x_down":
             position = 0
-            file = pd.concat(
-                [file, pd.DataFrame(
+            entry_blotter = pd.concat(
+                [entry_blotter, pd.DataFrame(
                     {
                         "DATE": [entry.index[i]],
-                        "SYMBOL1": [stock_a],
+                        "SYMBOL": [stock_a],
                         "ACTION": ['BUY'],
                         "SIZE": [size_a],
                         "PRICE": [entry.iloc[i][stock_a+"_Open"]],
                         "TRIP": [trip],
                         "LMT_PRICE": [lmt_price_a],
-                        "STATUS": [lmt_status_a],
-                        "SYMBOL2": [stock_b],
-                        "ACTION2": ['SELL'],
-                        "SIZE2": [size_b],
-                        "PRICE2": [entry.iloc[i][stock_b + "_Open"]],
-                        "TRIP2": [trip],
-                        "LMT_PRICE2": [lmt_price_b],
-                        "STATUS2": [lmt_status_b],
+                        "STATUS": [lmt_status_a]
                     }
                 )]
             )
-    file = file.set_index("DATE")
-    file.to_csv("action.csv")
+            entry_blotter = pd.concat(
+                [entry_blotter, pd.DataFrame(
+                    {
+                        "DATE": [entry.index[i]],
+                        "SYMBOL": [stock_b],
+                        "ACTION": ['SELL'],
+                        "SIZE": [size_b],
+                        "PRICE": [entry.iloc[i][stock_b + "_Open"]],
+                        "TRIP": [trip],
+                        "LMT_PRICE": [lmt_price_b],
+                        "STATUS": [lmt_status_b]
+                    }
+                )]
+            )
+
+    return entry_blotter
 
 
 if __name__ == "__main__":
@@ -147,7 +176,8 @@ if __name__ == "__main__":
 
     # data & table
     # all_data = get_full_signal(file_name, moving_average_num, std_level, stockA, stockB)
-    get_table(file_name, moving_average_num, std_level, stockA, stockB, sizeA, sizeB, "Entry",
+    calculate_entry_orders(file_name, moving_average_num, std_level, stockA, stockB, sizeA, sizeB,
+      "Entry",
               lmt_priceA, lmt_statusA, lmt_priceB, lmt_statusB)
 
 
